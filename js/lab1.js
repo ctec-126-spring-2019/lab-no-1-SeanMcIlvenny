714bd65867cd5fbe09f17411ccb5468e65e21deb
Sean McIlvenny <smcilvenny22@gmail.com>
2019-04-21 21:46:54 -0700

    completed

--- js/lab1.js
+++ js/lab1.js
@@ -5,25 +5,27 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
-    let output = "<h2>Bruce's Skills<h2>";
+    let skills = ['Web Development','Gardening','Drumming','Volunteering']; //missing end bracket
+    let output = "<h2>Bruce's Skills</h2>"; //missing closing tag on h2
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
-        output += '<li>' + skills[i] + '</li>';
+    output += '<li>' + skills[i] + '</li>';
     }
     output += '</ul>';
     document.getElementById('skills').innerHTML = '<p>' + output + '</p>';
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+    let firstName = 'Bruce'; //missing semi colon and equal sign on variable assignment
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    let collegeName = 'Clark College'; //missing semi colon
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+    //output line was missing $ on lastName, city, and gpa
 
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
+    //quotes need to be same 
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
